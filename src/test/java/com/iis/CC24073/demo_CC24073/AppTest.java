14e67a8fda50e0e6bc10160adb93bbaffdaf9237
Endernoob09 <cc24073@ues.edu.sv>
2024-05-05 22:42:01 -0600

    Primer commit

---
+++ src/test/java/com/iis/CC24073/demo_CC24073/AppTest.java
@@ -0,0 +1,20 @@
+package com.iis.CC24073.demo_CC24073;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testHello()
+    {
+        assertTrue( true );
+    }
+}
